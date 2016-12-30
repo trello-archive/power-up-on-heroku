ddfa945e6568012c5f41f5cb612f4d51ee6695a6
bentleycook <bentley.s.cook@gmail.com>
2016-12-30 16:18:42 -0500

    Need CORS access from Heroku and missing NPM Fields
    * Trello can't get the manifest without correct CORS settings
    * NPM expected repository field and valid SPDX License value

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 // Set port based on environment variable
 // https://nodejs.org/api/process.html#process_process_env
 app.set('port', (process.env.PORT || 5000));
+
+// Allow Trello to make requests
+// https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
+app.use(cors({ origin: 'https://trello.com' }));
 
 // Serve static files out of the `public` directory
 // https://expressjs.com/en/starter/static-files.html
